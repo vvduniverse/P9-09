9d75401e37b111c3ed845667790c072104a2df7b
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-18 12:08:33 +0200

    Initial Commit

---
+++ P9-09/P9_09App.swift
@@ -0,0 +1,17 @@
+//
+//  P9_09App.swift
+//  P9-09
+//
+//  Created by Vahtee Boo on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P9_09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
